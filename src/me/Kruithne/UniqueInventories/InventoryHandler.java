b845b43a36e409d0110111426aea011c527d894e
Kruithne <personal@stanleybatch.co.uk>
2012-03-03 21:20:23 +0000

    Added flatpacking of inventories and exportation to database.

--- src/me/Kruithne/UniqueInventories/InventoryHandler.java
+++ src/me/Kruithne/UniqueInventories/InventoryHandler.java
@@ -1,6 +1,11 @@
 package me.Kruithne.UniqueInventories;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 
 public class InventoryHandler {
 
@@ -13,7 +18,44 @@ public class InventoryHandler {
 	
 	public void saveInventory(Player player)
 	{
-		this.database.query(String.format("INSERT INTO uniqueinventories (playerName, worldName, gamemodeID, experience) VALUES('%s', '%s', %s, %s) ON DUPLICATE KEY UPDATE Experience = %4$s", player.getName(), player.getWorld().getName(), player.getGameMode(), player.getExp()));
+		this.database.query(
+			String.format("INSERT INTO uniqueInventories (playerName, worldName, gamemodeID, inventory, experience) VALUES('%s', '%s', '%s', '%s', %s) ON DUPLICATE KEY UPDATE Experience = %5$s", 
+				player.getName(),
+				player.getWorld().getName(),
+				player.getGameMode(),
+				this.flatPackInventory(player.getInventory()),
+				player.getExp()
+			)
+		);
+	}
+	
+	private String flatPackInventory(Inventory playerInventory)
+	{
+		ArrayList<String> itemData = new ArrayList<String>();
+		Iterator<ItemStack> itemStackIterator = playerInventory.iterator();
+		
+		while (itemStackIterator.hasNext())
+		{
+			ItemStack theItem = itemStackIterator.next();
+			itemData.add(String.format("%s:%s:%s:%s", theItem.getTypeId(), theItem.getAmount(), theItem.getDurability(), theItem.getData().getData()));
+		}
+		
+		return Join(itemData, ",");
+	}
+	
+	private String Join(ArrayList<String> coll, String delimiter)
+	{
+	    if (coll.isEmpty())
+	    	return "";
+	 
+	    StringBuilder sb = new StringBuilder();
+	 
+	    for (String x : coll)
+	    	sb.append(x + delimiter);
+	 
+	    sb.delete(sb.length()-delimiter.length(), sb.length());
+	 
+	    return sb.toString();
 	}
 	
 }
