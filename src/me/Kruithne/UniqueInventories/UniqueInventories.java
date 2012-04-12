8b83862d65a9643611acf42a699dffea28aabd10
Morten Nilsen <morten@runsafe.no>
2012-04-13 01:09:18 +0200

    Whitespace fixes

--- src/me/Kruithne/UniqueInventories/UniqueInventories.java
+++ src/me/Kruithne/UniqueInventories/UniqueInventories.java
@@ -1,35 +1,34 @@
 package me.Kruithne.UniqueInventories;
 
-import java.io.InputStream;
-
 import no.runsafe.framework.RunsafePlugin;
 import no.runsafe.framework.configuration.IConfigurationDefaults;
 import no.runsafe.framework.configuration.IConfigurationFile;
 
+import java.io.InputStream;
+
 public class UniqueInventories extends RunsafePlugin implements IConfigurationFile, IConfigurationDefaults
 {
+	public PlayerListener playerListener = null;
 
-    public PlayerListener playerListener = null;
+	@Override
+	protected void PluginSetup()
+	{
+		addComponent(InventoryHandler.class);
+		addComponent(PlayerListener.class);
+		addComponent(InventoryRepository.class);
+		addComponent(DebugCommand.class);
+		addComponent(InventoryUniverses.class);
+	}
 
-    @Override
-    protected void PluginSetup()
-    {
-        addComponent(InventoryHandler.class);
-        addComponent(PlayerListener.class);
-        addComponent(InventoryRepository.class);
-        addComponent(DebugCommand.class);
-        addComponent(InventoryUniverses.class);
-    }
+	@Override
+	public String getConfigurationPath()
+	{
+		return "plugins/" + this.getName() + "/config.yml";
+	}
 
-    @Override
-    public String getConfigurationPath()
-    {
-        return "plugins/" + this.getName() + "/config.yml";
-    }
-
-    @Override
-    public InputStream getDefaultConfiguration()
-    {
-        return getResource("defaults.yml");
-    }
+	@Override
+	public InputStream getDefaultConfiguration()
+	{
+		return getResource("defaults.yml");
+	}
 }
