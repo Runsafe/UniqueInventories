ef5e3d4ceb048dbfb7e2c3a1273e81dc183fec66
Morten Nilsen <morten@runsafe.no>
2013-01-03 00:47:16 +0100

    timer set to 100 ticks, block interact events while limited

--- src/no/runsafe/UniqueInventories/PlayerListener.java
+++ src/no/runsafe/UniqueInventories/PlayerListener.java
@@ -2,24 +2,29 @@ package no.runsafe.UniqueInventories;
 
 import no.runsafe.framework.event.player.IPlayerChangedWorldEvent;
 import no.runsafe.framework.event.player.IPlayerDropItemEvent;
+import no.runsafe.framework.event.player.IPlayerInteractEvent;
 import no.runsafe.framework.event.player.IPlayerMove;
 import no.runsafe.framework.server.RunsafeLocation;
 import no.runsafe.framework.server.RunsafeWorld;
 import no.runsafe.framework.server.event.player.RunsafePlayerChangedWorldEvent;
 import no.runsafe.framework.server.event.player.RunsafePlayerDropItemEvent;
+import no.runsafe.framework.server.event.player.RunsafePlayerInteractEvent;
 import no.runsafe.framework.server.player.RunsafePlayer;
 import no.runsafe.framework.timer.ForegroundWorker;
 import no.runsafe.framework.timer.IScheduler;
 
 import java.util.ArrayList;
 
-public class PlayerListener extends ForegroundWorker<String, RunsafePlayer> implements IPlayerChangedWorldEvent, IPlayerDropItemEvent, IPlayerMove
+public class PlayerListener
+	extends ForegroundWorker<String, RunsafePlayer>
+	implements IPlayerChangedWorldEvent, IPlayerDropItemEvent, IPlayerMove, IPlayerInteractEvent
 {
 	public PlayerListener(IScheduler scheduler, InventoryHandler inventoryHandler, IUniverses universes)
 	{
 		super(scheduler);
 		this.inventoryHandler = inventoryHandler;
 		this.universes = universes;
+		setInterval(100);
 	}
 
 	@Override
@@ -44,6 +49,13 @@ public class PlayerListener extends ForegroundWorker<String, RunsafePlayer> impl
 	public void OnPlayerDropItem(RunsafePlayerDropItemEvent event)
 	{
 		if (this.limitedPlayers.contains(event.getPlayer().getName()))
+			event.setCancelled(true);
+	}
+
+	@Override
+	public void OnPlayerInteractEvent(RunsafePlayerInteractEvent event)
+	{
+		if (limitedPlayers.contains(event.getPlayer().getName()))
 			event.setCancelled(true);
 	}
 
